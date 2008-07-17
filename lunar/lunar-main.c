4b56ef7a8cd53348b1dc1ff5b6f5f52db23414bb
Wu Xiaotian <yetist@gmail.com>
2008-07-17 08:27:03 +0000

    2008-07-17  yetist  <yetist@gmail.com>
        * lunar/lunar-main.c:
        * lunar/lunar.h:
        * lunar/Makefile.am:
        * ChangeLog:
        * configure.ac:
        完善了一下--disable-gtk编译参数。

--- lunar/lunar-main.c
+++ lunar/lunar-main.c
@@ -21,8 +21,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330,
  * Boston, MA 02111-1307, USA.
  * */
-
-#include <config.h>
+#ifdef HAVE_CONFIG_H
+    #include <config.h>
+#endif
 #include <glib/gi18n-lib.h>
 #include "lunar-main.h"
 
--- lunar/lunar.h
+++ lunar/lunar.h
@@ -25,8 +25,16 @@
 #ifndef __LUNAR_H__
 #define __LUNAR_H__  1
 
+#ifdef HAVE_CONFIG_H
+    #include <config.h>
+#endif
+
 #include <lunar/lunar-date.h>
-#include <lunar/lunar-calendar.h>
+
+#ifdef ENABLE_GTK
+    #include <lunar/lunar-calendar.h>
+#endif
+
 #include <lunar/lunar-main.h>
 
 #endif /*__LUNAR_H__ */
