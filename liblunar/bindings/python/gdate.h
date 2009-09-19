ce02db4249682eb4b61bdf0a3bac9fd3587c5d65
Wu Xiaotian <yetist@gmail.com>
2009-09-19 15:40:31 +0000

    add gdate.h, update po, update python demo

---
+++ liblunar/bindings/python/gdate.h
@@ -0,0 +1,16 @@
+typedef enum
+{
+  G_DATE_BAD_MONTH = 0,
+  G_DATE_JANUARY   = 1,
+  G_DATE_FEBRUARY  = 2,
+  G_DATE_MARCH     = 3,
+  G_DATE_APRIL     = 4,
+  G_DATE_MAY       = 5,
+  G_DATE_JUNE      = 6,
+  G_DATE_JULY      = 7,
+  G_DATE_AUGUST    = 8,
+  G_DATE_SEPTEMBER = 9,
+  G_DATE_OCTOBER   = 10,
+  G_DATE_NOVEMBER  = 11,
+  G_DATE_DECEMBER  = 12
+} GDateMonth;
--- liblunar/bindings/python/lunardate-demo.py
+++ liblunar/bindings/python/lunardate-demo.py
@@ -2,12 +2,12 @@
 # coding=utf-8
 import sys
 import os
+import time
 sys.path.insert(1, os.path.join(os.getcwd(), ".libs"));
 
 import liblunar
 print liblunar.__file__
-
-print dir (liblunar)
+#print dir (liblunar)
 
 import locale
 locale.setlocale(locale.LC_ALL, "")
@@ -15,16 +15,16 @@ locale.setlocale(locale.LC_ALL, "")
 def test_date(y, m, d, t):
     l=liblunar.Date()
     l.set_solar_date(y, m, d, t)
-    format=(
-            "%(YEAR)年%(MONTH)月%(DAY)",
-            "%(year)年%(month)月%(day)日",
-            "%(NIAN)年%(YUE)月%(RI)日%(SHI)时",
-            "%(nian)年%(yue)月%(ri)日%(shi)时",
-            "%(Y60)年%(M60)月%(D60)日%(H60)时",
-            "%(Y8)年%(M8)月%(D8)日%(H8)时",
-            "%(shengxiao)")
-    for i in format:
-        print l.strftime(i)
+    format={"%(YEAR)年%(MONTH)月%(DAY)日%(HOUR)时":"%(YEAR)年%(MONTH)月%(DAY)日%(HOUR)时",
+            "%(year)年%(month)月%(day)日%(hour)时": "%(year)年%(month)月%(day)日%(hour)时",
+            "%(NIAN)年%(YUE)月%(RI)日%(SHI)时": "%(NIAN)年%(YUE)月%(RI)日%(SHI)时",
+            "%(nian)年%(yue)月%(ri)日%(shi)时": "%(nian)年%(yue)月%(ri)日%(shi)时",
+            "%(Y60)年%(M60)月%(D60)日%(H60)时": "%(Y60)年%(M60)月%(D60)日%(H60)时",
+            "%(Y8)年%(M8)月%(D8)日%(H8)时": "%(Y8)年%(M8)月%(D8)日%(H8)时",
+            "生肖属%(shengxiao)": "生肖属%(shengxiao)"}
+    for i in format.keys():
+        print i,"\t"*2, l.strftime(format[i])
     l.free()
 
-test_date(2009, 3, 18, 20)
+t = time.localtime()
+test_date(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour)
