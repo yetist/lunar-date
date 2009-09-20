ab894377b00cd25a3c45f918cc88e304141dde3f
Wu Xiaotian <yetist@gmail.com>
2009-09-20 14:43:16 +0000

    update __init__.py

--- liblunar/bindings/python/__init__.py
+++ liblunar/bindings/python/__init__.py
@@ -1,23 +1,25 @@
-# -*- Mode: Python -*-
-# pygobject - Python bindings for the GObject library
-# Copyright (C) 2008  Johan Dahlin
-#
-#   gio/__init__.py: initialisation file for gio module
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-# USA
+#! /usr/bin/env python
+# -*- encoding:utf-8 -*-
+# FileName: __init__.py
+
+"This file is part of liblunar"
+ 
+__author__   = "yetist"
+__copyright__= "Copyright (C) 2009 yetist <yetist@gmail.com>"
+__license__  = """
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 from _lunardate import *
 del _lunardate
