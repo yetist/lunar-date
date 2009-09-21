f21445fd930461d6fb1cca2a23848e65b86acda2
Wu Xiaotian <yetist@gmail.com>
2009-09-21 06:58:10 +0000

    add version info for liblunar

--- liblunar/lunar/lunar-main.c
+++ liblunar/lunar/lunar-main.c
@@ -41,3 +41,53 @@ void	lunar_init				 (int	 *argc, char ***argv)
 	bindtextdomain (GETTEXT_PACKAGE, LIBLUNAR_LOCALEDIR);
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 }
+
+const guint liblunar_major_version = LIBLUNAR_MAJOR_VERSION;
+const guint liblunar_minor_version = LIBLUNAR_MINOR_VERSION;
+const guint liblunar_micro_version = LIBLUNAR_MICRO_VERSION;
+
+/**
+ * lunar_check_version:
+ * @required_major: the required major version.
+ * @required_minor: the required minor version.
+ * @required_micro: the required micro version.
+ *
+ * Checks that the Liblunar library in use is compatible with the
+ * given version. Generally you would pass in the constants
+ * #LIBLUNAR_MAJOR_VERSION, #LIBLUNAR_MINOR_VERSION, #LIBLUNAR_MICRO_VERSION
+ * as the three arguments to this function; that produces
+ * a check that the library in use is compatible with
+ * the version of Liblunar the application or module was compiled
+ * against.
+ *
+ * Compatibility is defined by two things: first the version
+ * of the running library is newer than the version
+ * @required_major.required_minor.@required_micro. Second
+ * the running library must be binary compatible with the
+ * version @required_major.required_minor.@required_micro
+ * (same major version.)
+ *
+ * Return value: %NULL if the Liblunar library is compatible with the
+ *   given version, or a string describing the version mismatch.
+ *   The returned string is owned by Liblunar and must not be modified
+ *   or freed.
+ *
+ **/
+const gchar *
+lunar_check_version (guint required_major,
+                    guint required_minor,
+                    guint required_micro)
+{
+  gint liblunar_effective_micro = 100 * LIBLUNAR_MINOR_VERSION + LIBLUNAR_MICRO_VERSION;
+  gint required_effective_micro = 100 * required_minor + required_micro;
+
+  if (required_major > LIBLUNAR_MAJOR_VERSION)
+    return "Liblunar version too old (major mismatch)";
+  if (required_major < LIBLUNAR_MAJOR_VERSION)
+    return "Liblunar version too new (major mismatch)";
+  if (required_effective_micro < liblunar_effective_micro - LIBLUNAR_BINARY_AGE)
+    return "Liblunar version too new (micro mismatch)";
+  if (required_effective_micro > liblunar_effective_micro)
+    return "Liblunar version too old (micro mismatch)";
+  return NULL;
+}
