aa24c2e22ac19c8993306e86bb5d2a0190abe5dd
Wu Xiaotian <yetist@gmail.com>
2011-03-06 14:45:51 +0000

    2011-03-06  yetist  <wuxiaotian@redflag-linux.com>
    	*  liblunar/example/python-binding.py: 
    	*  liblunar/example/seed.js: 
    	*  liblunar/example/gjs.js: 
    	modify scripts.

--- example/gjs.js
+++ example/gjs.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env gjs-console
 const Gettext = imports.gettext;
 const GLib = imports.gi.GLib;
 const LunarDate = imports.gi.LunarDate;
@@ -8,6 +9,7 @@ Gettext.textdomain("liblunar");
 LunarDate.init(0, "");
 let l = new LunarDate.Date();
 l.set_solar_date(2010,4,2,18);
+//var out = l.strftime("%(NIAN)年%(YUE)月%(RI)日%(SHI)时");
 let out = l.strftime("%(NIAN)nian%(YUE)yue%(RI)ri%(SHI)shi");
 print(out);
 l.free();
---
+++ example/seed.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env seed
+Gettext = imports.gettext;
+GLib = imports.gi.GLib;
+LunarDate = imports.gi.LunarDate;
+
+Gettext.bindtextdomain("liblunar", "/usr/share/locale");
+Gettext.textdomain("liblunar");
+
+LunarDate.init(0, "");
+var l = new LunarDate.Date();
+l.set_solar_date(2010,4,2,18);
+var out = l.strftime("%(NIAN)年%(YUE)月%(RI)日%(SHI)时");
+print(out);
+l.free();
