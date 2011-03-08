9b907247a8a1c3a72ca178f8249c598cec690156
Wu Xiaotian <yetist@gmail.com>
2011-03-08 15:36:32 +0000

    update

--- lunar-date/date.h
+++ lunar-date/date.h
@@ -1,6 +1,6 @@
-#ifndef __LUNAR_H__
-#define __LUNAR_H__  1
+#ifndef __DATE_H__
+#define __DATE_H__  1
 
 #include <lunar-date/lunar-date.h>
 #include <lunar-date/lunar-main.h>
-#endif /*__LUNAR_H__ */
+#endif /*__DATE_H__ */
