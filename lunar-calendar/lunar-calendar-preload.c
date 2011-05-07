a0dce9f6df4f367048f5065201389cfbd233e801
Wu Xiaotian <yetist@gmail.com>
2011-05-07 13:37:57 +0000

    update lunar-calendar2

--- lunar-calendar/lunar-calendar-preload.c
+++ lunar-calendar/lunar-calendar-preload.c
@@ -22,7 +22,7 @@
 #include <stdlib.h>
 #include <dlfcn.h>
 #include <gtk/gtk.h>
-#include <calendar.h>
+#include <lunar-calendar/lunar-calendar.h>
 
 GtkWidget* gtk_calendar_new (void)
 {
--- tests/test-calendar.c
+++ tests/test-calendar.c
@@ -21,7 +21,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <gtk/gtk.h>
-#include <calendar.h>
+#include <lunar-calendar/lunar-calendar.h>
 
 #define DEF_PAD 12
 #define DEF_PAD_SMALL 6
