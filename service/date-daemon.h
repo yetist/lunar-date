ffc3beda011dda9f40ce5c23ee7bf15238e29b25
Wu Xiaotian <yetist@gmail.com>
2019-11-26 13:06:03 +0800

    update copyright to 2019

--- service/date-daemon.h
+++ service/date-daemon.h
@@ -1,26 +1,26 @@
 /* vi: set sw=4 ts=4 wrap ai: */
 /*
- * date-daemon.h: This file is part of ____
+ * date-daemon.h: This file is part of lunar-date.
  *
- * Copyright (C) 2016 yetist <yetist@yetibook>
+ * Copyright (C) 2016-2019 yetist <yetist@gmail.com>
  *
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  * */
 
-#ifndef __DATE_DAEMON_H__ 
+#ifndef __DATE_DAEMON_H__
 #define __DATE_DAEMON_H__  1
 
 #include <glib-object.h>
