46ab1dad4dc6fb64dd9d0453a36f52e522e961be
Wu Xiaotian <yetist@gmail.com>
2021-05-26 10:50:27 +0800

    fix test fail in build directory.

--- tests/testing.c
+++ tests/testing.c
@@ -33,17 +33,59 @@ static const struct {
 	GDateDay day;
 	guint8 hour;
 	const gchar *format;
-	const gchar *value;
+	const gchar *raw_value;
+	const gchar *local_value;
 } solar_array[] = {
-	{1910, 2, 19, 2, "%(YEAR)年%(MONTH)月%(DAY)日%(HOUR)时", "一九一○年二月十九日二时"},
-	{1910, 2, 19, 2, "%(year)年%(month)月%(day)日%(hour)时", "1910年2月19日2时"},
-	{1910, 2, 19, 2, "%(NIAN)年%(YUE)月%(RI)日%(SHI)时", "庚戌年一月初十日丑时"},
-	{1910, 2, 19, 2, "%(nian)年%(yue)月%(ri)日%(shi)时", "1910年1月10日1时"},
-	{1910, 2, 19, 2, "%(Y60)年%(M60)月%(D60)日%(H60)时", "庚戌年戊寅月乙卯日丁丑时"},
-	{1910, 2, 19, 2, "%(Y8)年%(M8)月%(D8)日%(H8)时", "庚戌年戊寅月乙卯日丁丑时"},
-	{1910, 2, 19, 2, "%(shengxiao)", "狗"},
-	{1910, 2, 19, 2, "%(holiday)", "雨水"},
+	{
+	  1910, 2, 19, 2,
+	  "%(YEAR)年%(MONTH)月%(DAY)日%(HOUR)时",
+	  "y\304\253ji\307\224y\304\253l\303\255ng\345\271\264\303\250r\346\234\210sh\303\255ji\307\224\346\227\245\303\250r\346\227\266",
+	  "一九一○年二月十九日二时"
+  },
+	{
+	  1910, 2, 19, 2,
+	  "%(year)年%(month)月%(day)日%(hour)时",
+	  "1910\345\271\2642\346\234\21019\346\227\2452\346\227\266",
+	  "1910年2月19日2时"
+  },
+	{
+	  1910, 2, 19, 2,
+	  "%(NIAN)年%(YUE)月%(RI)日%(SHI)时",
+	  "G\304\223ngX\305\253\345\271\264Jan\346\234\210Ch\305\253sh\303\255\346\227\245Ch\307\222u\346\227\266",
+	  "庚戌年一月初十日丑时"
+  },
+	{
+	  1910, 2, 19, 2,
+	  "%(nian)年%(yue)月%(ri)日%(shi)时",
+	  "1910\345\271\2641\346\234\21010\346\227\2451\346\227\266",
+	  "1910年1月10日1时"
+  },
+	{
+	  1910, 2, 19, 2,
+	  "%(Y60)年%(M60)月%(D60)日%(H60)时",
+	  "G\304\223ngX\305\253\345\271\264W\303\271Y\303\255n\346\234\210Y\307\220M\307\216o\346\227\245D\304\253ngCh\307\222u\346\227\266",
+	  "庚戌年戊寅月乙卯日丁丑时"
+  },
+	{
+	  1910, 2, 19, 2,
+	  "%(Y8)年%(M8)月%(D8)日%(H8)时",
+	  "G\304\223ngX\305\253\345\271\264W\303\271Y\303\255n\346\234\210Y\307\220M\307\216o\346\227\245D\304\253ngCh\307\222u\346\227\266",
+	  "庚戌年戊寅月乙卯日丁丑时"
+  },
+	{
+	  1910, 2, 19, 2,
+	  "%(shengxiao)",
+	  "Dog",
+	  "狗"
+  },
+	{
+	  1910, 2, 19, 2,
+	  "%(holiday)",
+	  "Y\307\224s",
+	  "雨水"
+  },
 };
+
 static const gint num_solar_array = G_N_ELEMENTS (solar_array);
 
 static const struct {
@@ -53,16 +95,57 @@ static const struct {
 	guint8 hour;
 	gboolean isleap;
 	const gchar *format;
-	const gchar *value;
+	const gchar *raw_value;
+	const gchar *local_value;
 } lunar_array[] = {
-	{1906, 8, 14, 8, 0, "%(YEAR)年%(MONTH)月%(DAY)日%(HOUR)时", "一九○六年一十月一日八时"},
-	{1906, 8, 14, 8, 0, "%(year)年%(month)月%(day)日%(hour)时", "1906年10月1日8时"},
-	{1906, 8, 14, 8, 0, "%(NIAN)年%(YUE)月%(RI)日%(SHI)时", "丙午年八月十四日辰时"},
-	{1906, 8, 14, 8, 0, "%(nian)年%(yue)月%(ri)日%(shi)时", "1906年8月14日4时"},
-	{1906, 8, 14, 8, 0, "%(Y60)年%(M60)月%(D60)日%(H60)时", "丙午年丁酉月戊寅日丙辰时"},
-	{1906, 8, 14, 8, 0, "%(Y8)年%(M8)月%(D8)日%(H8)时", "丙午年丁酉月戊寅日丙辰时"},
-	{1906, 8, 14, 8, 0, "%(shengxiao)", "马"},
-	{1906, 8, 14, 8, 0, "%(holiday)", "国庆节"},
+	{
+	  1906, 8, 14, 8, 0,
+	  "%(YEAR)年%(MONTH)月%(DAY)日%(HOUR)时",
+	  "y\304\253ji\307\224l\303\255ngli\303\271\345\271\264y\304\253sh\303\255\346\234\210y\304\253\346\227\245b\304\201\346\227\266",
+	  "一九○六年一十月一日八时"
+  },
+	{
+	  1906, 8, 14, 8, 0,
+	  "%(year)年%(month)月%(day)日%(hour)时",
+	  "1906\345\271\26410\346\234\2101\346\227\2458\346\227\266",
+	  "1906年10月1日8时"
+  },
+	{
+	  1906, 8, 14, 8, 0,
+	  "%(NIAN)年%(YUE)月%(RI)日%(SHI)时",
+	  "B\307\220ngW\307\224\345\271\264Aug\346\234\210Sh\303\255s\303\254\346\227\245Ch\303\251n\346\227\266",
+	  "丙午年八月十四日辰时"
+  },
+	{
+	  1906, 8, 14, 8, 0,
+	  "%(nian)年%(yue)月%(ri)日%(shi)时",
+	  "1906\345\271\2648\346\234\21014\346\227\2454\346\227\266",
+	  "1906年8月14日4时"
+  },
+	{
+	  1906, 8, 14, 8, 0,
+	  "%(Y60)年%(M60)月%(D60)日%(H60)时",
+	  "B\307\220ngW\307\224\345\271\264D\304\253ngY\307\222u\346\234\210W\303\271Y\303\255n\346\227\245B\307\220ngCh\303\251n\346\227\266",
+	  "丙午年丁酉月戊寅日丙辰时"
+  },
+	{
+	  1906, 8, 14, 8, 0,
+	  "%(Y8)年%(M8)月%(D8)日%(H8)时",
+	  "B\307\220ngW\307\224\345\271\264D\304\253ngY\307\222u\346\234\210W\303\271Y\303\255n\346\227\245B\307\220ngCh\303\251n\346\227\266",
+	  "丙午年丁酉月戊寅日丙辰时"
+  },
+	{
+	  1906, 8, 14, 8, 0,
+	  "%(shengxiao)",
+	  "Horse",
+	  "马"
+  },
+	{
+	  1906, 8, 14, 8, 0,
+	  "%(holiday)",
+	  "\345\233\275\345\272\206\350\212\202",
+	  "国庆节"
+  },
 };
 
 static const gint num_lunar_array = G_N_ELEMENTS (lunar_array);
@@ -85,6 +168,17 @@ static void test_lunar_date(void);
 static void test_holiday_date(void);
 static void test_custom_holiday(void);
 
+static gboolean is_installed(void)
+{
+	const char *result;
+	result = g_dgettext ("lunar-date", "y\304\253");
+
+	if (g_strcmp0 (result, "一") == 0) {
+		return TRUE;
+	}
+	return FALSE;
+}
+
 static void test_solar_date(void)
 {
 	gint i;
@@ -100,7 +194,11 @@ static void test_solar_date(void)
 		if (error != NULL)
 			g_error_free(error);
 		value = lunar_date_strftime(date, solar_array[i].format);
-		g_assert_cmpstr (solar_array[i].value, ==, value);
+		if (is_installed()) {
+			g_assert_cmpstr (solar_array[i].local_value, ==, value);
+		} else {
+			g_assert_cmpstr (solar_array[i].raw_value, ==, value);
+		}
 		g_free (value);
 	}
 	lunar_date_free(date);
@@ -122,7 +220,11 @@ void test_lunar_date(void)
 		if (error != NULL)
 			g_error_free(error);
 		value = lunar_date_strftime(date, lunar_array[i].format);
-		g_assert_cmpstr (lunar_array[i].value, ==, value);
+		if (is_installed()) {
+			g_assert_cmpstr (lunar_array[i].local_value, ==, value);
+		} else {
+			g_assert_cmpstr (lunar_array[i].raw_value, ==, value);
+		}
 		g_free (value);
 	}
 	lunar_date_free(date);
